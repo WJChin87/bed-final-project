820c168ac2244be7d15b6d55f08230cc45ec6b30
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-26 19:07:50 +0200

    feat: add hostRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import loginRouter from "./routes/login.js";
 import userRouter from "./routes/users.js";
+import hostRouter from "./routes/hosts.js";
 import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
+app.use("/hosts", hostRouter);
 
 app.use("/login", loginRouter);
 
