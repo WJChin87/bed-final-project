cfdab61222826494794ad400c55f9dfe57f5b922
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-26 14:15:27 +0200

    feat: add login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import loginRouter from "./routes/login.js";
+import "dotenv/config";
 
 const app = express();
+app.use(express.json());
+
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
