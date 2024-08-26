5482ccc2d53f8f56711210f9ed49c08ab191778a
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-26 16:16:14 +0200

    feat: add createUser to users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import getUsers from "../services/users/getUsers.js";
+import createUser from "../services/users/createUser.js";
 
 const router = Router();
 
@@ -8,6 +9,36 @@ router.get("/", async (req, res, next) => {
     const users = await getUsers();
     res.json(users);
   } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+
+    const newUser = await createUser(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture
+    );
+
+    if (newUser) {
+      res.status(201).json({
+        message: `User with id ${newUser.id} successfully added`,
+        user: newUser,
+      });
+    } else {
+      res.status(400).json({
+        message: "User creation error",
+      });
+    }
+  } catch (error) {
+    console.error("User creation error:", error);
     next(error);
   }
 });
