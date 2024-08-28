eb3c96e17b044525edd6839156c67bec945b754d
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-28 18:03:52 +0200

    feat: add filter function

--- src/services/bookings/getBookings.js
+++ src/services/bookings/getBookings.js
@@ -2,14 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const getBookings = async () => {
-  try {
-    const bookings = await prisma.booking.findMany();
-    return bookings;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+const getBookings = async (filters = {}) => {
+  const prisma = new PrismaClient();
+
+  const where = {};
+
+  if (filters.userId) where.userId = filters.userId;
+  if (filters.propertyId) where.propertyId = filters.propertyId;
+
+  return await prisma.booking.findMany({
+    where,
+  });
 };
 
 export default getBookings;
