489b94e4c28667981dd62248957447349534f686
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-28 12:50:34 +0200

    buld: function  to delete  review by id

---
+++ src/services/reviews/deleteReviewById.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const deleteReviewById = async (id) => {
+  try {
+    const review = await prisma.review.delete({
+      where: { id },
+    });
+    return review;
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      error.code === "P2025" // Assuming 'P2025' is the correct error code for record not found
+    ) {
+      return null; // Review with the specified ID was not found
+    } else {
+      throw new Error(`Error in deleting review: ${error.message}`);
+    }
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export default deleteReviewById;
