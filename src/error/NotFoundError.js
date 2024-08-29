91f9b094b2730f3a4e3edb6f0eac85117b3e8665
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-29 13:18:20 +0200

    build: not found error

---
+++ src/error/NotFoundError.js
@@ -0,0 +1,9 @@
+class NotFoundError extends Error {
+  constructor(resourceType, id) {
+    super(`${resourceType} with id ${id} was not found!`);
+
+    this.name = "NotFoundError";
+  }
+}
+
+export default NotFoundError;
