045b474bdc499147586c6aa05c25df85a6bd7b63
Wing_Joe <wingjoe.chin@gmail.com>
2024-08-30 13:54:44 +0200

    feat: reverted back to original code

--- src/services/users/getUserById.js
+++ src/services/users/getUserById.js
@@ -1,6 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import NotFoundError from "../../errors/NotFoundError.js";
 
 const prisma = new PrismaClient();
+
+// const getUserById = async (id) => {
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: { id },
+//     });
+//     return user;
+//   } catch (error) {
+//     throw error;
+//   } finally {
+//     await prisma.$disconnect();
+//   }
+// };
+
+// export default getUserById;
 
 const getUserById = async (id) => {
   try {
